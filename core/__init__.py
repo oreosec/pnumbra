279f13b472d8e090123c0b849edc69d38403bda4
Dipkill <tolol@protonmail.com>
2020-05-24 20:19:18 -0700

    Python3

--- core/__init__.py
+++ core/__init__.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 
-from core.arpspoof import *
-from core.dnsspoof import *
 from core.config.config import *
 from core.httpserver import *
-from core.sniffer import *
-from core.dump import *
 from core.banner import *
 from core.begin import *
 from core.accesspoint import *
